c8a12274c3a841ae6dda140f27270848319c25b7
PouceMine07mm <83622140+PouceMine07mm@users.noreply.github.com>
2022-08-07 17:03:14 -0400

    Suppression de la fonctionnalité d'améliorer l'incrément

--- javascript/script.js
+++ javascript/script.js
@@ -4,24 +4,11 @@ var somme = 0;
 //Valeur par defaut de lincrement
 var incrementeur = 1;
 
-//Variable memorisant le niveau de lamelioration
-var niveau = 1;
-
 //Fonction qui incremente la variable 'somme'
 function incrementSomme() {
     somme += incrementeur
     document.getElementById("somme").innerHTML = "Clique moi!" + " (+" + incrementeur +")" 
-    document.getElementById("total").innerHTML = "Alumettes: " +somme
-    document.getElementById("amelioration").innerHTML = "niveau actuel: " + niveau
-}
-
-//Fonction qui incremente lincrementeur
-function incrementIncrementeur() {
-    incrementeur *= 1.5
-    niveau ++
-    incrementeur = Math.round(incrementeur)
-    document.getElementById("amelioration").innerHTML = "niveau actuel: " + niveau
-    document.getElementById("somme").innerHTML = "Clique moi!" + " (+" + incrementeur +")" 
     document.getElementById("total").innerHTML = somme
 }
+
 
