86e371d948733c2bb4815e2ef51dbb6c195f012f
PouceMine07mm <83622140+PouceMine07mm@users.noreply.github.com>
2022-08-07 17:34:14 -0400

    Ajout variables bois, souffre, portefeuille

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,28 @@ var somme = 0;
 //Valeur par defaut de lincrement
 var incrementeur = 1;
 
+//Argent amasse par joueur
+var portefeuille =0;
+
+//Qt des deux ressources detenus par joueur
+var boisQt = 100
+
+var souffreQt = 100
+
+//prix des deux ressources a lachat par le joueur
+var boisPrix = 10
+
+var souffrePrix = 10
+
+
+
+
+
+
+
+
+
+
 //Fonction qui incremente la variable 'somme'
 function incrementSomme() {
     somme += incrementeur
