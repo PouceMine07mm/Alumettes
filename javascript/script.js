69fd03445078c82b916e8cc0dfe6c9de7635548a
PouceMine07mm <83622140+PouceMine07mm@users.noreply.github.com>
2022-08-07 18:23:58 -0400

    Ajout fonction faire alumettes

--- javascript/script.js
+++ javascript/script.js
@@ -7,30 +7,33 @@ var incrementeur = 1;
 //Argent amasse par joueur
 var portefeuille =0;
 
-//Qt des deux ressources detenus par joueur
-var boisQt = 100
+//Quantite des deux ressources detenus par joueur
+var boisQt = 1
 
-var souffreQt = 100
+var souffreQt = 10
 
 //prix des deux ressources a lachat par le joueur
 var boisPrix = 10
 
 var souffrePrix = 10
 
+// TODO Faire en sorte les chiffres ronds n'aient pas de ".0"
+//Fonction qui deduit les quantites d'inventaire
+function faireAlumette(){
+        boisQt -= 0.10
+        souffreQt -= 1
+        boisQt = Number.parseFloat(boisQt).toFixed(1);
+        document.getElementById("boisInventaire").innerHTML = boisQt
+        document.getElementById("souffreInventaire").innerHTML = souffreQt
+        incrementSomme()
 
-
-
-
-
-
-
-
+    }
+}
 
 //Fonction qui incremente la variable 'somme'
 function incrementSomme() {
     somme += incrementeur
-    document.getElementById("somme").innerHTML = "Faire une alumette"
-    document.getElementById("total").innerHTML = `Alumettes: ${somme}`
+    document.getElementById("total").innerHTML = somme
 }
 
 
