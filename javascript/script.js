e0ccfdd7d7aeb82d4141fbb2247bfab31f6d3591
PouceMine07mm <83622140+PouceMine07mm@users.noreply.github.com>
2022-08-08 14:13:55 -0400

    Ajout constantes valeurs par defaut

--- javascript/script.js
+++ javascript/script.js
@@ -11,22 +11,32 @@ setInterval(verifSouffre, 100)
 //Somme totale des unites
 var somme = 0;
 
+const sommeDefaut = 0;
 //Valeur par defaut de lincrement
 var incrementeur = 1;
 
+const incrementaireDefaut = 1;
 //Argent amasse par joueur
 var portefeuille =100000;
+var portefeuille =1000;
 
+const portefeuilleDefaut = 1000;
 //Quantite des deux ressources detenus par joueur
 var boisQt = 1000
 
+const boisQtDefaut = 1000;
+
 var souffreQt = 100
 
+const souffreQtDefaut = 100;
 //prix des deux ressources a lachat par le joueur
 var boisPrix = 10
 
+const boisPrixDefaut = 100;
+
 var souffrePrix = 10
 
+const souffreprixDefaut = 100;
 // TODO Faire en sorte les chiffres ronds n'aient pas de ".0"
 //Fonction qui deduit les quantites d'inventaire
 function faireAlumette(){
