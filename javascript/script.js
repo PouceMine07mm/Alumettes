880005fd70194ffaafd5765d51dd0d76f8474bbe
PouceMine07mm <83622140+PouceMine07mm@users.noreply.github.com>
2022-08-07 22:31:14 -0400

    Ajout possibilité d'acheter du bois et du souffre
    N'est pas terminé

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
+//Minuterie qui verifie si il est possible de fabriquer une alumette au 0.5s
+
+setInterval(verifAlumette, 500)
+
 //Somme totale des unites
 var somme = 0;
 
@@ -5,12 +9,12 @@ var somme = 0;
 var incrementeur = 1;
 
 //Argent amasse par joueur
-var portefeuille =0;
+var portefeuille =100000;
 
 //Quantite des deux ressources detenus par joueur
-var boisQt = 1
+var boisQt = 1000
 
-var souffreQt = 10
+var souffreQt = 100
 
 //prix des deux ressources a lachat par le joueur
 var boisPrix = 10
@@ -20,16 +24,16 @@ var souffrePrix = 10
 // TODO Faire en sorte les chiffres ronds n'aient pas de ".0"
 //Fonction qui deduit les quantites d'inventaire
 function faireAlumette(){
-    if ( boisQt > 0 ){
-        boisQt -= 0.10
+    if ( boisQt > 0 && souffreQt > 0){
+        boisQt -= 10
         souffreQt -= 1
-        boisQt = Number.parseFloat(boisQt).toFixed(1);
+        // boisQt = Number.parseFloat(boisQt).toFixed(1);
         document.getElementById("boisInventaire").innerHTML = boisQt
         document.getElementById("souffreInventaire").innerHTML = souffreQt
         incrementSomme()
 
     } else {
-        document.getElementById("somme").disabled = true
+        document.getElementById("creeAlumette").disabled = true
     }
 }
 
@@ -39,4 +43,30 @@ function incrementSomme() {
     document.getElementById("total").innerHTML = somme
 }
 
+// Fonction pour l'achat du bois
+function achatBois(quantite){
+        boisQt += (quantite * 10)
+        portefeuille -= quantite * boisPrix
+        document.getElementById("boisInventaire").innerHTML = boisQt
+        document.getElementById("argent").innerHTML = portefeuille
+}
+
+// Fonction pour l'achat du souffre
+
+function achatSouffre(quantite){
+        souffreQt += quantite
+        portefeuille -= quantite * boisPrix
+        document.getElementById("souffreInventaire").innerHTML = souffreQt
+        document.getElementById("argent").innerHTML = portefeuille
+}
+
+// Fonction verifiant si les materiaux necessaire pour la construction dune alumette sont present
+
+function verifAlumette(){
+    if ( boisQt > 0 && souffreQt > 0){
+        document.getElementById("creeAlumette").disabled = false
+    } else {
+        document.getElementById("creeAlumette").disabled = true
+    }
+}
 
