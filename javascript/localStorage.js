6b21dbe89183b23619d2acb11d03ea248a2a76e0
PouceMine07mm <83622140+PouceMine07mm@users.noreply.github.com>
2022-08-15 22:31:10 -0400

    Fragmentation des fonctions de sauvegardes/chargement/initialisation

--- javascript/localStorage.js
+++ javascript/localStorage.js
@@ -1,3 +1,6 @@
+// Fonction qui lors du chargement de la page si le localstorage contient de l'information
+// et donc de soit l'initialiser si il n'en contient pas ou bien dans le cas échéant de charger 
+// la sauvegarde
 function chargementPage(){
     if  ( localStorage.length === 0 )  {
         initLocalStorage()
@@ -5,44 +8,92 @@ function chargementPage(){
         charger()
     }
 }
-
+// Fonction qui va sauvegarder toutes les données dans le localstorage du furteur
 function sauvegarde() {
+    sauvegardeAlumettesLocalStorage()
+    sauvegardeArgentLocalStorage()
+    sauvegardeBoisLocalStorage()
+    sauvegardeSouffreLocalStorage()
+}
+// Fonction qui va permettre de prendre les données du localstorage pour les afficher
+function charger() {
+    chargerAlumettesLocalStorage()
+    chargerArgentLocalStorage()
+    chargerBoisLocalStorage()
+    chargerSouffreLocalStorage()
+}
+// Fonction qui remet tout le localstorage aux valeurs initiales
+function initLocalStorage() {
+    initAlumettesLocalStorage()
+    initArgentLocalStorage()
+    initBoisLocalStorage()
+    initSouffreLocalStorage()
+}
+
+function sauvegardeAlumettesLocalStorage(){
     localStorage.setItem("nbrAlumettes", somme)
-    localStorage.setItem("nbrBois", boisQt)
-    localStorage.setItem("nbrSouffre", souffreQt)
+}
+
+function chargerAlumettesLocalStorage(){
+    somme = localStorage.getItem("nbrAlumettes")
+    somme = parseInt(somme)
+    document.getElementById("total").innerHTML = somme
+}
+
+function initAlumettesLocalStorage(){
+    localStorage.setItem("nbrAlumettes", sommeDefaut)
+    somme = localStorage.getItem("nbrAlumettes")
+    somme = parseInt(somme)
+    document.getElementById("total").innerHTML = somme
+}
+
+function sauvegardeArgentLocalStorage(){
     localStorage.setItem("argent", portefeuille)
 }
 
-function charger() {
-    somme = localStorage.getItem("nbrAlumettes")
-    boisQt = localStorage.getItem("nbrBois")
-    souffreQt = localStorage.getItem("nbrSouffre")
+function chargerArgentLocalStorage(){
     portefeuille = localStorage.getItem("argent")
-    somme = parseInt(somme)
-    boisQt = parseInt(boisQt)
-    souffreQt = parseInt(souffreQt)
     portefeuille = parseInt(portefeuille)
-    document.getElementById("total").innerHTML = somme
-    document.getElementById("boisInventaire").innerHTML = boisQt
-    document.getElementById("souffreInventaire").innerHTML = souffreQt
     document.getElementById("argent").innerHTML = portefeuille
 }
 
-function initLocalStorage() {
-    localStorage.setItem("nbrAlumettes", sommeDefaut)
-    localStorage.setItem("nbrBois", boisQtDefaut)
-    localStorage.setItem("nbrSouffre", souffreQtDefaut)
+function initArgentLocalStorage(){
     localStorage.setItem("argent", portefeuilleDefaut)
-    somme = localStorage.getItem("nbrAlumettes")
-    boisQt = localStorage.getItem("nbrBois")
-    souffreQt = localStorage.getItem("nbrSouffre")
     portefeuille = localStorage.getItem("argent")
-    somme = parseInt(somme)
-    boisQt = parseInt(boisQt)
-    souffreQt = parseInt(souffreQt)
     portefeuille = parseInt(portefeuille)
-    document.getElementById("total").innerHTML = somme
-    document.getElementById("boisInventaire").innerHTML = boisQt
-    document.getElementById("souffreInventaire").innerHTML = souffreQt
     document.getElementById("argent").innerHTML = portefeuille
 }
+
+function sauvegardeBoisLocalStorage(){
+    localStorage.setItem("nbrBois", boisQt)
+}
+
+function chargerBoisLocalStorage(){
+    boisQt = localStorage.getItem("nbrBois")
+    boisQt = parseInt(boisQt)
+    document.getElementById("boisInventaire").innerHTML = boisQt
+}
+function initBoisLocalStorage() {
+    localStorage.setItem("nbrBois", boisQtDefaut)
+    boisQt = localStorage.getItem("nbrBois")
+    boisQt = parseInt(boisQt)
+    document.getElementById("boisInventaire").innerHTML = boisQt
+}
+
+function sauvegardeSouffreLocalStorage(){
+    localStorage.setItem("nbrSouffre", souffreQt)
+}
+
+function chargerSouffreLocalStorage(){
+    souffreQt = localStorage.getItem("nbrSouffre")
+    souffreQt = parseInt(souffreQt)
+    document.getElementById("souffreInventaire").innerHTML = souffreQt
+}
+
+function initSouffreLocalStorage(){
+    localStorage.setItem("nbrSouffre", souffreQtDefaut)
+    souffreQt = localStorage.getItem("nbrSouffre")
+    souffreQt = parseInt(souffreQt)
+    document.getElementById("souffreInventaire").innerHTML = souffreQt
+}
+
